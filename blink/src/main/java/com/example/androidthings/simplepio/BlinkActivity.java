47d591796cf4b0045057ae15e027ffac3ff98779
Anthony Fermin <afermin@fuzzproductions.com>
2017-04-11 15:00:39 -0400

    Add Countdown with beep

--- blink/src/main/java/com/example/androidthings/simplepio/BlinkActivity.java
+++ blink/src/main/java/com/example/androidthings/simplepio/BlinkActivity.java
@@ -17,46 +17,75 @@
 package com.example.androidthings.simplepio;
 
 import android.app.Activity;
-import com.google.android.things.pio.Gpio;
-import com.google.android.things.pio.PeripheralManagerService;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+
+import com.google.android.things.contrib.driver.apa102.Apa102;
+import com.google.android.things.contrib.driver.ht16k33.AlphanumericDisplay;
+import com.google.android.things.contrib.driver.pwmspeaker.Speaker;
+import com.google.android.things.contrib.driver.rainbowhat.RainbowHat;
+import com.google.android.things.pio.Gpio;
 
 import java.io.IOException;
 
 /**
  * Sample usage of the Gpio API that blinks an LED at a fixed interval defined in
- * {@link #INTERVAL_BETWEEN_BLINKS_MS}.
- *
+ * {@link #intervalBetweenBlinksMs}.
+ * <p>
  * Some boards, like Intel Edison, have onboard LEDs linked to specific GPIO pins.
  * The preferred GPIO pin to use on each board is in the {@link BoardDefaults} class.
- *
  */
 public class BlinkActivity extends Activity {
     private static final String TAG = BlinkActivity.class.getSimpleName();
-    private static final int INTERVAL_BETWEEN_BLINKS_MS = 1000;
+    private static final int MINIMUM = 1000;
+    private static final long DURATION = 10000;
+    private static final int QUARTER = 250;
+    public int intervalBetweenBlinksMs = 1000;
+    public int counter = 60;
 
     private Handler mHandler = new Handler();
-    private Gpio mLedGpio;
-    private boolean mLedState = false;
+    private Gpio green;
+    private Gpio red;
+    private Gpio blue;
+    private boolean ledState = false;
+    private boolean piezoState = false;
+    private AlphanumericDisplay display;
+    private Speaker piezo;
+    private Apa102 strip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(TAG, "Starting BlinkActivity");
+        init();
+//        cleanup();
+        countdown();
+    }
 
-        PeripheralManagerService service = new PeripheralManagerService();
+    private void init() {
         try {
-            String pinName = BoardDefaults.getGPIOForLED();
-            mLedGpio = service.openGpio(pinName);
-            mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
-            Log.i(TAG, "Start blinking LED GPIO pin");
-            // Post a Runnable that continuously switch the state of the GPIO, blinking the
-            // corresponding LED
-            mHandler.post(mBlinkRunnable);
+            green = RainbowHat.openLed(RainbowHat.LED_GREEN);
+            red = RainbowHat.openLed(RainbowHat.LED_RED);
+            blue = RainbowHat.openLed(RainbowHat.LED_BLUE);
+            piezo = RainbowHat.openPiezo();
+            display = RainbowHat.openDisplay();
+            strip = RainbowHat.openLedStrip();
         } catch (IOException e) {
-            Log.e(TAG, "Error on PeripheralIO API", e);
+            e.printStackTrace();
+        }
+    }
+
+    private void cleanup() {
+        try {
+            green.close();
+            red.close();
+            blue.close();
+            piezo.close();
+            display.close();
+            strip.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -68,11 +97,40 @@ public class BlinkActivity extends Activity {
         // Close the Gpio pin.
         Log.i(TAG, "Closing LED GPIO pin");
         try {
-            mLedGpio.close();
+            green.close();
+            red.close();
+            blue.close();
+            piezo.close();
+            display.close();
+            strip.close();
         } catch (IOException e) {
             Log.e(TAG, "Error on PeripheralIO API", e);
         } finally {
-            mLedGpio = null;
+            green = null;
+            red = null;
+            blue = null;
+            piezo = null;
+            display = null;
+            strip = null;
+        }
+    }
+
+    private void defaultBlink() {
+        try {
+            green.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+            red.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+            blue.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+            int[] rainbow = new int[7];
+            for (int i = 0; i < rainbow.length; i++) {
+                rainbow[i] = Color.HSVToColor(255, new float[]{i * 360.f / rainbow.length, 1.0f, 1.0f});
+            }
+            strip.write(rainbow);
+            Log.i(TAG, "Start blinking LED GPIO pin");
+            // Post a Runnable that continuously switch the state of the GPIO, blinking the
+            // corresponding LED
+            mHandler.post(mBlinkRunnable);
+        } catch (IOException e) {
+            Log.e(TAG, "Error on PeripheralIO API", e);
         }
     }
 
@@ -80,19 +138,79 @@ public class BlinkActivity extends Activity {
         @Override
         public void run() {
             // Exit Runnable if the GPIO is already closed
-            if (mLedGpio == null) {
+            if (green == null || red == null || blue == null) {
                 return;
             }
+
             try {
                 // Toggle the GPIO state
-                mLedState = !mLedState;
-                mLedGpio.setValue(mLedState);
-                Log.d(TAG, "State set to " + mLedState);
+                ledState = !ledState;
+                green.setValue(ledState);
+                red.setValue(ledState);
+                blue.setValue(ledState);
 
-                // Reschedule the same runnable in {#INTERVAL_BETWEEN_BLINKS_MS} milliseconds
-                mHandler.postDelayed(mBlinkRunnable, INTERVAL_BETWEEN_BLINKS_MS);
+                Log.d(TAG, "State set to " + ledState);
+
+                // Reschedule the same runnable in {#intervalBetweenBlinksMs} milliseconds
+                mHandler.postDelayed(mBlinkRunnable, intervalBetweenBlinksMs);
             } catch (IOException e) {
                 Log.e(TAG, "Error on PeripheralIO API", e);
+            }
+        }
+    };
+
+    private void countdown() {
+        mHandler.post(countdownRunnable);
+        mHandler.post(piezoRunnable);
+    }
+
+    private Runnable countdownRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (display == null || piezo == null) {
+                return;
+            }
+
+            try {
+                display.display(counter);
+                display.setEnabled(true);
+
+                if (counter == 0) {
+                    defaultBlink();
+                } else {
+                    mHandler.postDelayed(countdownRunnable, MINIMUM);
+                }
+                counter--;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    private Runnable piezoRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (piezo == null) {
+                return;
+            }
+
+            int delay;
+            try {
+                piezoState = !piezoState;
+                if (piezoState) {
+                    piezo.play(784.3);
+                    delay = QUARTER;
+                } else {
+                    piezo.stop();
+                    delay = QUARTER * 3;
+                }
+                if (counter < 0) {
+                    piezo.stop();
+                } else {
+                    mHandler.postDelayed(piezoRunnable, delay);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     };
