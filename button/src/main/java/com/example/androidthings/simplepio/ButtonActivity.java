af5ffb08a98bce69377285c068074037ca4077f7
Anthony Fermin <afermin@fuzzproductions.com>
2017-08-08 17:04:36 -0400

    Fix crash when both buttons are pressed at once

--- button/src/main/java/com/example/androidthings/simplepio/ButtonActivity.java
+++ button/src/main/java/com/example/androidthings/simplepio/ButtonActivity.java
@@ -27,7 +27,6 @@ import java.io.IOException;
 
 /**
  * Sample usage of the Gpio API that logs when a button is pressed.
- *
  */
 public class ButtonActivity extends DeviceInfoActivity {
     private static final String TAG = ButtonActivity.class.getSimpleName();
@@ -36,6 +35,7 @@ public class ButtonActivity extends DeviceInfoActivity {
     private Button mButtonB;
     private Button mButtonC;
     private Servo servo;
+    private boolean buttonPressed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,15 +67,18 @@ public class ButtonActivity extends DeviceInfoActivity {
                 @Override
                 public void onButtonEvent(Button button, boolean pressed) {
                     try {
-                        if (pressed) {
+                        if (!buttonPressed && pressed) {
+                            buttonPressed = true;
                             servo.setEnabled(true);
                             servo.setAngle(servo.getMaximumAngle());
-                        } else {
+                        } else if (!pressed){
                             servo.setEnabled(false);
+                            buttonPressed = false;
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+
                 }
             });
         } catch (IOException e) {
@@ -86,7 +89,7 @@ public class ButtonActivity extends DeviceInfoActivity {
     private void connectButtonB() {
         try {
             String pinName = "BCM20";
-            mButtonC = RainbowHat.openButton(pinName);
+            mButtonB = RainbowHat.openButton(pinName);
         } catch (IOException e) {
             Log.e(TAG, "Error on PeripheralIO API", e);
         }
@@ -95,16 +98,18 @@ public class ButtonActivity extends DeviceInfoActivity {
     private void connectButtonC() {
         try {
             String pinName = "BCM16";
-            mButtonB = RainbowHat.openButton(pinName);
-            mButtonB.setOnButtonEventListener(new Button.OnButtonEventListener() {
+            mButtonC = RainbowHat.openButton(pinName);
+            mButtonC.setOnButtonEventListener(new Button.OnButtonEventListener() {
                 @Override
                 public void onButtonEvent(Button button, boolean pressed) {
                     try {
-                        if (pressed) {
+                        if (!buttonPressed && pressed) {
+                            buttonPressed = true;
                             servo.setEnabled(true);
                             servo.setAngle(servo.getMinimumAngle());
-                        } else {
+                        } else if (!pressed){
                             servo.setEnabled(false);
+                            buttonPressed = false;
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -119,9 +124,59 @@ public class ButtonActivity extends DeviceInfoActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        closeButtonA();
+        closeButtonB();
+        closeButtonC();
+        closeServo();
+    }
+
+    private void closeServo() {
+        if (servo != null) {
+            // Close the PWM pin
+            Log.i(TAG, "Closing Servo - PWM pin");
+            try {
+                servo.setEnabled(false);
+                servo.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error on PeripheralIO API", e);
+            } finally {
+                mButtonA = null;
+            }
+        }
+    }
+
+    private void closeButtonC() {
+        // Close the Gpio pin
+        if (mButtonC != null) {
+            Log.i(TAG, "Closing Button C - GPIO pin: BCM16");
+            try {
+                mButtonC.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error on PeripheralIO API", e);
+            } finally {
+                mButtonC = null;
+            }
+        }
+    }
+
+    private void closeButtonB() {
+        // Close the Gpio pin
+        if (mButtonB != null) {
+            Log.i(TAG, "Closing Button B - GPIO pin: BCM20");
+            try {
+                mButtonB.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error on PeripheralIO API", e);
+            } finally {
+                mButtonB = null;
+            }
+        }
+    }
+
+    private void closeButtonA() {
+        // Close the Gpio pin
         if (mButtonA != null) {
-            // Close the Gpio pin
-            Log.i(TAG, "Closing Button GPIO pin");
+            Log.i(TAG, "Closing Button A - GPIO pin: BCM21");
             try {
                 mButtonA.close();
             } catch (IOException e) {
